53ad725304497dfd185cac327e6f6c66b0e91988
dkent600 <github@douglaskent.com>
2025-08-16 08:11:50 -0600

    remove obsolete files

--- test-env.js
+++
@@ -1,3 +0,0 @@
-console.log('MEXC_API_KEY:', process.env.MEXC_API_KEY ? 'SET' : 'NOT SET');
-console.log('MEXC_API_SECRET:', process.env.MEXC_API_SECRET ? 'SET' : 'NOT SET');
-console.log('NODE_ENV:', process.env.NODE_ENV);
